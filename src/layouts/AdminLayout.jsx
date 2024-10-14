55a97b2a2970b26b5500be0e2eb874cae65d8a0c
Mariam-111 <mm8133278@gmail.com>
2024-10-14 17:39:51 +0300

    Updates in Routes

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AllUsers from "../pages/AllUsers";
 import Dashboard from "../pages/Dashboard";
 import NavbarDark from "../components/NavbarDark";
